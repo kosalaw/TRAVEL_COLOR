d5dd88b22d416ab0cead98fe955d5669692d59a0
Afsaneh SametTehrani <afsaneh.samettehrani@essec.edu>
2021-06-07 16:08:51 +0100

    added methods

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,15 +1,22 @@
 class CountriesController < ApplicationController
-
   def index
-
+    @countries = Country.all
   end
 
   def show
-
+    @country = Country.find(params[:id])
   end
 
   def compare
-
+    if params['/countries/compare'][:country_1].present?
+      @country_1 = Country.find(params['/countries/compare'][:country_1].to_i)
+    end
+    if params['/countries/compare'][:country_2].present?
+      @country_2 = Country.find(params['/countries/compare'][:country_2].to_i)
+    end
+    if params['/countries/compare'][:country_3].present?
+      @country_3 = Country.find(params['/countries/compare'][:country_3].to_i)
+    end
+    @countries = Country.all
   end
-
 end
