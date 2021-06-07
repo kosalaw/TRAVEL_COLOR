c003dca38416444efb7bc502d2bf521932d9e051
Afsaneh SametTehrani <afsaneh.samettehrani@essec.edu>
2021-06-07 18:01:55 +0100

    populated the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+Country.destroy_all
+User.destroy_all
+
+puts 'create countries'
+
+country_1 = Country.create!(name: 'France',
+iso: 'French',
+status: 'Open',
+quarantine: 'Yes',
+test: 'Yes',
+content: 'La France" redirects here. For other uses of "La France", see Lafrance. For other uses of "France", see France (disambiguation).',
+government_url: 'https://www.gouvernement.fr/en/news',
+color: 'Amber',
+upcoming_changes: 'N/A')
+
+country_2 = Country.create!(name: 'Italy',
+iso: 'it',
+status: 'Closed',
+quarantine: 'Yes',
+test: 'Yes',
+content: 'Italy applies health-related restriction measures to incoming travellers, which may vary depending on their country of origin.',
+government_url: 'http://www.italia.it/en/useful-info/covid-19-updates-information-for-tourists.html',
+color: 'Red',
+upcoming_changes: 'N/A')
+
+country_3 = Country.create!(name: 'Germany',
+iso: 'German',
+status: 'Closed',
+quarantine: 'Yes',
+test: 'No',
+content: 'This article is about the country. For other uses, see Germany (disambiguation) and Deutschland (disambiguation).
+"Federal Republic of Germany" redirects here. For the country from 1949 to 1990',
+government_url: 'https://www.bundesregierung.de/breg-en',
+color: 'Green',
+upcoming_changes: 'N/A')
+
+puts 'done creating countries'
+puts 'create users'
+
+user_1 = User.create!(email: 'test@test.com',
+password: "123456",
+first_name: 'Kosala',
+last_name: 'Doe')
+
+user_2 = User.create!(email: 'test1@test.com',
+password: "123456",
+first_name: 'Hugo',
+last_name: 'Smith')
+
+user_3 = User.create!(email: 'test2@test.com',
+password: "123456",
+first_name: 'Alex',
+last_name: 'Rodrigo')
+
+puts 'done creating users'
