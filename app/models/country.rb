317d085ec175b39bcdd98305c68e4ca6350f3146
Afsaneh SametTehrani <afsaneh.samettehrani@essec.edu>
2021-06-07 13:05:14 +0100

    added validation

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,4 @@
 class Country < ApplicationRecord
+  has_many :reviews
+  has_many :alerts
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :country
+
+  validates :stars, presence: true
+  validates :content, presence: true
 end
