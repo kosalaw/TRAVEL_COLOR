36cfc71304203db563f502441b8e95ee427c4ae7
Afsaneh SametTehrani <afsaneh.samettehrani@essec.edu>
2021-06-08 19:08:29 +0100

    changed authorization for compare view

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,5 +1,5 @@
 class CountriesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :compare]
   def index
     @countries = Country.all
   end
