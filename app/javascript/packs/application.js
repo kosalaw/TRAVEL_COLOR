3238da80191b8a1c62cb4ec4116d7747b02ca903
Kosala Wijesena <kosalabakk@yahoo.co.uk>
2021-06-08 19:15:02 +0200

    show page as of 08/06 1914

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,9 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import submitToggle from "./toggle.js";
+
+
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -31,4 +34,5 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-});
+  submitToggle();
+ });
