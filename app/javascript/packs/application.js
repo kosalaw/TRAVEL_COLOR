f0f6708d0659ca99c42c182e533bd92375f692f9
Rodrigo Fernando <souza-fernandouk@hotmail.com>
2021-06-09 10:50:30 +0100

    frontpage style

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,9 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import { loadDynamicBannerText } from '../components/banner';
+import { initMapbox } from '../components/mapbox';
+
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -31,4 +34,6 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  loadDynamicBannerText();
+  initMapbox();
 });
