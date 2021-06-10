d822571d9889945adcf669289df86e7d7c41991b
Afsaneh SametTehrani <afsaneh.samettehrani@essec.edu>
2021-06-10 21:38:02 +0100

    added customized colors

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,17 @@
 class Country < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :alerts, dependent: :destroy
+
+  def color_code
+    case color
+    when 'Amber'
+      "#ff8800"
+    when 'Green'
+      "#55a630"
+    when 'Red'
+      "#cc0022"
+    else
+      color
+    end
+  end
 end
