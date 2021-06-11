e16e596c4e05c4f327fd458996793917ddcfec67
Hugo Speak <hugo.speak@hotmail.com>
2021-06-11 14:36:13 +0100

    Map now on index page

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,6 +2,23 @@ class CountriesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show, :compare, :explore]
   def index
     @countries = Country.all
+
+    countries = Country.all
+    @temp_array = {}
+    countries.each do |country|
+      if country.status == nil || country.status == " "
+          @temp_array[country.name] = {
+            id: country.id,
+            status: country.status,
+            test: country.test,
+            quarantine: country.quarantine,
+            color: country.color,
+            upcoming_changes: country.upcoming_changes,
+            content: country.content
+          }
+      end
+    end
+    @json_array = @temp_array.to_json
   end
 
   def show
