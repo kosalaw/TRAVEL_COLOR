c2a953755458c238edc50f8d88ebad82baac0073
Hugo Speak <hugo.speak@hotmail.com>
2021-06-14 15:35:55 +0100

    added countries name to hover box

--- app/javascript/components/home-map.js
+++ app/javascript/components/home-map.js
@@ -19,56 +19,90 @@ const initMap = () => {
     var myJsArray = JSON.parse(document.getElementById("my_dataviz").dataset.countries)
     console.log(myJsArray);
 
+
+    let onClick = function(d) {
+      console.log(d);
+      d3.select('#title').html(''); // Empty old data from the info page
+      d3.select('#color').html(''); // Empty old data from the info page
+      d3.select('#status').html(''); // Empty old data from the info page
+      d3.select('#content').html(''); // Empty old data from the info page
+      d3.select('#test').html(''); // Empty old data from the info page
+      d3.select('#quarantine').html(''); // Empty old data from the info page
+      d3.select('#link').html(''); // Empty old data from the info page
+
+      d3.select('#title').html(d.properties.name);
+      if (myJsArray[d.properties.name]) {
+        // d3.select('#status').html(myJsArray[d.properties.name].["status"];
+        d3.select('#test').html("Requires test no older than: " + myJsArray[d.properties.name]["test"]);
+        d3.select('#quarantine').html("At destination isolate for: " + myJsArray[d.properties.name]["quarantine"]);
+        d3.select('#color').html("UK quarantine tier color: " + myJsArray[d.properties.name]["color"]);
+        // d3.select('#content').html(myJsArray[d.properties.name]["content"]);
+        let id = myJsArray[d.properties.name]["id"];
+        d3.select("#link").html(`<a href="/countries/${id}">More Info</a>`);
+      }
+      else {
+        d3.select('#status').html("Closed to Tourists");
+        let id = myJsArray[d.properties.name]["id"];
+        d3.select("#link").html(`<a href="/countries/${id}">More Info</a>`);
+
+      }
+    }
+
+    // let mouseOver = function(d) {
+    //     d3.select(this).style("opacity", 1).style("stroke", "black");
+    //     console.log(d.properties.name)
+    //     return tooltip.style("hidden", false).html(d.name);
+
+    //   }
+    // let mouseLeave = function(d) {
+    //   d3.select(this)
+    //     // .transition()
+    //     // .duration(200)
+    //     .style("opacity", 0.8)
+    //     .style("stroke", "white")
+    // }
+
+    let tooltip = d3.select("div.tooltip");
+
     d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson", function (error, data) {
       // if(error) return console.error(error);
 
       console.log(data.features)
       // Draw the map
-      g.append("g")
-        .attr("id", "countries")
-        .selectAll("path") // Select path
+      g.selectAll("path") // Select path
         .data(data.features)  // Bind geodata
         .enter()
-        .append("path")  // Add svg path for each country
-        .attr("country", function (d) { return d.properties.name; })
+        // Add svg path for each country
+        .append("path")
+        .attr("class", "country")
+        .attr("name", function (d) { return d.properties.name; })
+        .attr("d", path) // Send geometry data to path function to plot points
         .attr("fill", function (d) {
-          // console.log(d.properties.name);
-          // console.log(myJsArray[d.properties.name]);
-
           if (myJsArray[d.properties.name]) {
             console.log(d.properties.name);
             console.log(myJsArray[d.properties.name]);
             return "#55A630";
-          }
-          else {
-            return "#BBB"
-          }
+          } else { return "#BBB" }
         })
         .style("stroke", "#fff")
-        .attr("d", path) // Send geometry data to path function to plot points
-        .on("click", function (d) {
-          console.log(d);
-          d3.select('#title').html(''); // Empty old data from the info page
-          d3.select('#color').html(''); // Empty old data from the info page
-          d3.select('#status').html(''); // Empty old data from the info page
-          d3.select('#content').html(''); // Empty old data from the info page
-          d3.select('#test').html(''); // Empty old data from the info page
-          d3.select('#quarantine').html(''); // Empty old data from the info page
+        .style("opacity", 0.8)
+        .on("click", onClick)
+        .on("mouseover",function(d,i){
+                d3.select(this).raise().style("opacity", 1).style("stroke", "black");
+                return tooltip.style("hidden", false).html(d.properties.name);
+            })
+            .on("mousemove",function(d){
+                tooltip.classed("hidden", false)
+                       .style("top", ((d3.event.pageY) - 300) + "px")
+                       .style("left", (d3.event.pageX + 10) + "px")
+                       .html(d.properties.name);
+            })
+            .on("mouseout",function(d,i){
+                d3.select(this).style("opacity", 0.8).style("stroke", "white");
+                tooltip.classed("hidden", true);
+            });
 
-          d3.select('#title').html(d.properties.name);
-          if (myJsArray[d.properties.name]) {
-            // d3.select('#status').html(myJsArray[d.properties.name].["status"];
-            d3.select('#test').html("Requires test no older than: " + myJsArray[d.properties.name]["test"]);
-            d3.select('#quarantine').html("At destination isolate for: " + myJsArray[d.properties.name]["quarantine"]);
-            d3.select('#color').html("UK quarantine tier color: " + myJsArray[d.properties.name]["color"]);
-            // d3.select('#content').html(myJsArray[d.properties.name]["content"]);
-            var id = myJsArray[d.properties.name]["id"];
-            d3.select("#link").html(`<a href="/countries/${id}">More Info</a>`);
-          }
-          else {
-            d3.select('#status').html("Closed to Tourists");
-          }
-        });
+
     })
   }
 }
