c1dcdfaee8f7d1eee15e9028cffa129d855684d9
Afsaneh SametTehrani <afsaneh.samettehrani@essec.edu>
2021-06-16 17:08:26 +0100

    add files css navbar

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,14 +2,17 @@ import Typed from "typed.js";
 
 let typedElement;
 const loadDynamicBannerText = () => {
+  if (document.querySelector("#banner-typed-text")) {
   typedElement = new Typed("#banner-typed-text", {
     strings: ["Travel Colour", "Travel restriction free", "Travel Covid free", "Travel free", "Travel with smile"],
     typeSpeed: 50,
     loop: true,
-  });
+  });}
 };
 const destroyDynamicBannerText = () => {
+  if (document.querySelector("#banner-typed-text")) {
   typedElement.destroy();
+  }
 };
 
 export { loadDynamicBannerText, destroyDynamicBannerText };
